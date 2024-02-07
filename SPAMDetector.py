01199e34031d1d8c88cdaa858ca2120350d826cb
Saurabh6307 <139581589+Saurabh6307@users.noreply.github.com>
2024-02-08 00:09:56 +0530

    Add files via upload
    Spam Classification Python

---
+++ SPAMDetector.py
@@ -0,0 +1,31 @@
+import pickle
+import streamlit as st
+from gtts import gTTS
+from playsound import playsound
+
+model = pickle.load(open("spam.pkl", "rb"))
+cv = pickle.load(open("vectorizer.pkl", "rb"))
+
+def main():
+    st.title("Email Spam Classification App")
+    st.subheader("Built With Streamlit & Python")
+    msg = st.text_input("Enter a Text: ")
+    if st.button("Predict"):
+        data = [msg]
+        vect = cv.transform(data).toarray()
+        prediction = model.predict(vect)
+        result = prediction[0]
+        if result == 1:
+            st.error("This is a spam mail")
+            
+            playsound("welcome.mp3")
+        else:
+            st.success("This is a ham mail")
+
+if __name__ == "__main__":
+
+
+   
+
+    main()
+    
