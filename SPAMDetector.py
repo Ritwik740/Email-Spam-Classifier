661cea92295cc658257ea3ef7d74cb31f18afbb5
RITWIK MISHRA <61270723+Ritwik740@users.noreply.github.com>
2024-02-08 21:32:05 +0530

    Update SPAMDetector.py

--- SPAMDetector.py
+++ SPAMDetector.py
@@ -1,13 +1,13 @@
 import pickle
 import streamlit as st
-import pygame
+import replit
+from replit import audio
 
 # Load the model and vectorizer
 model = pickle.load(open("spam.pkl", "rb"))
 cv = pickle.load(open("vectorizer.pkl", "rb"))
 
-# Initialize Pygame's mixer module
-pygame.mixer.init()
+
 
 def main():
     st.title("Email Spam Classification App")
@@ -20,9 +20,7 @@ def main():
         result = prediction[0]
         if result == 1:
             st.error("This is a spam mail")
-            # Load and play the sound file
-            pygame.mixer.music.load("https://github.com/Ritwik740/Email-Spam-Classifier/blob/fc1aee4f5474014e7f788db3e8babbe077f40f6d/welcome.mp3")
-            pygame.mixer.music.play()
+            audio.play_file("welcome.mp3")
         else:
             st.success("This is a ham mail")
 
