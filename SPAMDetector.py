aa34a1a4d451bf0715697ad5694e9f988e6893cd
Ritwik740 <61270723+Ritwik740@users.noreply.github.com>
2024-02-11 11:33:45 +0000

    updated homepage

--- SPAMDetector.py
+++ SPAMDetector.py
@@ -9,7 +9,7 @@ cv = pickle.load(open("vectorizer.pkl", "rb"))
 def main():
   page_bg_img = '''
   <style>
-   
+
 [class="main st-emotion-cache-uf99v8 ea3mdgi8"]{
   background-image: url("https://images.unsplash.com/photo-1562813733-b31f71025d54?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   background-size: cover;
@@ -32,6 +32,16 @@ def main():
 
     else:
       st.success("This is a ham mail")
+  st.write(
+      "To know more about Spamming and spam emails, you can visit the following links:"
+  )
+  st.write(
+      "[Spamming](https://en.wikipedia.org/wiki/Spamming) | [Spam Emails](https://en.wikipedia.org/wiki/Email_spam)"
+  )
+
+  st.write(
+      "Contact: +91 6307463343 | [Linkedin](https://linkedin.com/in/saurabh-mishra-638276278) | [Github](https://github.com/Saurabh6307)"
+  )
 
 
 if __name__ == "__main__":
